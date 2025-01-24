cd679d7e0b39239343436c43443097ef4c7c8a1d
ShiveshLallPics <shiveshlallpics@gmail.com>
2025-01-24 20:30:37 +0000

    Nav 1.1 Popup

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle'); // Match button's class
-    const nav = document.querySelector('nav'); // Select <nav>
-
-    menuToggle.addEventListener('click', function () {
-        nav.classList.toggle('show-menu'); // Toggle class on nav
-    });
-});
