c67ee512e3f5c24a1d50c43cc79c383d5833bf98
ShiveshLallPics <shiveshlallpics@gmail.com>
2025-01-24 19:49:27 +0000

    Nav update1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
     document.addEventListener('DOMContentLoaded', function () {
-        const menuToggle = document.querySelector('.menu-toggle');
+        const menuToggle = document.querySelector('menu-toggle');
         const nav = document.querySelector('nav');
 
         menuToggle.addEventListener('click', function () {
