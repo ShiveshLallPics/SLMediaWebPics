13fcb4813ce7e172b2258a577bb303a62c36bf84
ShiveshLallPics <shiveshlallpics@gmail.com>
2025-01-24 19:02:19 +0000

    Mobile and server changes

--- Public/script.js
+++
@@ -1,2 +0,0 @@
-// This is a placeholder for future JavaScript functionality
-console.log('Your photography website is ready to shine!');
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'Public')));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '/')));
 
 // Handle requests to the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'Public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
